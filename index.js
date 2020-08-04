f9ed96e16d201d5272a99af10795eb8e0740c0e3
IliaKheifetc <kheyfetc.ilia@gmail.com>
2020-08-05 02:54:04 +0300

    fixing response

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const { Telegraf } = require("telegraf");
+const express = require("express");
+
+const expressApp = express();
 
 const bot = new Telegraf(process.env.BOT_TOKEN, {
   // Telegram options
@@ -31,8 +34,19 @@ bot.telegram.setWebhook(
   "https://tfs-observer-telegram-bot.herokuapp.com/telegraf/07e4f521f4a38e9e50e08b3f8525efe23fc556fa9b6cb75ad2b987a612fce3e9"
 );
 
-bot.startWebhook(
-  "/telegraf/07e4f521f4a38e9e50e08b3f8525efe23fc556fa9b6cb75ad2b987a612fce3e9",
-  null,
-  process.env.PORT
+// bot.startWebhook(
+//   "/telegraf/07e4f521f4a38e9e50e08b3f8525efe23fc556fa9b6cb75ad2b987a612fce3e9",
+//   null,
+//   process.env.PORT
+// );
+
+expressApp.get("/", (req, res) => res.send("Hello World!"));
+expressApp.use(
+  bot.webhookCallback(
+    "/telegraf/07e4f521f4a38e9e50e08b3f8525efe23fc556fa9b6cb75ad2b987a612fce3e9"
+  )
 );
+
+expressApp.listen(process.env.PORT, () => {
+  console.log(`app listening on port ${process.env.PORT}!`);
+});
