c96f9efa9b601fc469130e621c404cad57090d28
IliaKheifetc <kheyfetc.ilia@gmail.com>
2020-08-05 00:48:49 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const { Telegraf } = require("telegraf");
+
+const bot = new Telegraf(process.env.BOT_TOKEN, {
+  // Telegram options
+  agent: null, // https.Agent instance, allows custom proxy, certificate, keep alive, etc.
+  webhookReply: true // Reply via webhook
+});
+// (async () => {
+//   try {
+//     const r = await bot.telegram.setWebhook(
+//       "https://tfs-observer-telegram-bot.herokuapp.com/"
+//     );
+//
+//     console.log("r", r);
+//
+//     try {
+//       const response = await bot.telegram.getWebhookInfo();
+//
+//       console.log("response", response);
+//     } catch (err) {
+//       console.error("error", err);
+//     }
+
+bot.command("/hello", ctx => {
+  ctx.reply("How are you doing?");
+});
+//   } catch (e) {
+//     console.log("error", e);
+//   }
+// })();
+
+bot.startWebhook(
+  "/d3cf3429a76b8ef1d2d94d826c27a7e2a04e8f10d5c6ec494e2055baea9bef17",
+  null,
+  4500
+);
+bot.telegram.setWebhook(
+  "https://tfs-observer-telegram-bot.herokuapp.com/telegraf/d3cf3429a76b8ef1d2d94d826c27a7e2a04e8f10d5c6ec494e2055baea9bef17"
+);
+
+// bot.launch({
+//   webhook: {
+//     domain:
+//       "https://tfs-observer-telegram-bot.herokuapp.com/telegraf/d3cf3429a76b8ef1d2d94d826c27a7e2a04e8f10d5c6ec494e2055baea9bef17",
+//     port: 4500
+//   }
+// });
