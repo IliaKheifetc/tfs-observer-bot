89ff4efdc745495b07cac6c99ed17c5c84ddb0f6
IliaKheifetc <kheyfetc.ilia@gmail.com>
2021-01-18 01:39:17 +0300

    fix returned fields for mutation response when adding subscribers to hasura db

--- mutations.js
+++ mutations.js
@@ -1,8 +1,6 @@
 const addSubscriber = `mutation ModifySubscribers($objects: [subscribers_insert_input!]!) {
   insert_subscribers(objects: $objects) {
-    id
-    name
-    chatId
+    affected_rows
   }
 }`;
 
